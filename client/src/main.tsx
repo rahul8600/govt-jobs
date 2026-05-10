58738c47344cf62ab0ffbdf4876b6ab4082a1dad
rahul8600 <84216079+rahul8600@users.noreply.github.com>
2026-05-10 21:49:59 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,3 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
-
-// Register Service Worker for push notifications + caching
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('[SW] Registered:', reg.scope))
-      .catch(err => console.log('[SW] Error:', err));
-  });
-}
