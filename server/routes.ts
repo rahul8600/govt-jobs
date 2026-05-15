16939575f61873264ab5f996a78687e2cd4cb11d
rahul8600 <84216079+rahul8600@users.noreply.github.com>
2026-05-15 10:10:10 +0530

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -723,18 +723,23 @@ Sitemap: ${baseUrl}/sitemap.xml
       }
 
       // Blog pages sitemap
-      const blogs = await storage.getAllBlogs();
-      for (const blog of blogs) {
-        const slug = blog.slug || blog.id;
-        const lastmod = blog.createdAt ? new Date(blog.createdAt).toISOString().split('T')[0] : new Date().toISOString().split('T')[0];
-        xml += `  <url>
+      try {
+        const blogRes = await fetch(`${baseUrl}/api/blogs`);
+        if (blogRes.ok) {
+          const blogs = await blogRes.json();
+          for (const blog of blogs) {
+            const slug = blog.slug || blog.id;
+            const lastmod = blog.created_at ? new Date(blog.created_at).toISOString().split('T')[0] : new Date().toISOString().split('T')[0];
+            xml += `  <url>
     <loc>${baseUrl}/blog/${slug}</loc>
     <lastmod>${lastmod}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.7</priority>
   </url>
 `;
-      }
+          }
+        }
+      } catch {}
 
       xml += `</urlset>`;
 
